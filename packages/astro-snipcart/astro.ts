c5d5e862d72a8c5be0bd7f81751b7437dfb234d9
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-04-28 20:43:36 +0000

    feature: add tailwind and CustomerSignIn.astro

--- packages/astro-snipcart/astro.ts
+++ packages/astro-snipcart/astro.ts
@@ -1,4 +1,5 @@
 export { default as SnipcartSetup } from './src/SnipcartSetup.astro';
+export { default as CustomerSignIn } from './src/CustomerSignIn.astro';
 export { default as CartTotalPrice } from './src/CartTotalPrice.astro';
 export { default as CartItemsCount } from './src/CartItemsCount.astro';
 export { default as CartCheckout } from './src/CartCheckout.astro';

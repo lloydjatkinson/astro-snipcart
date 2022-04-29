561a2cbf32c06af441acb5320ec7fe1ff0230c27
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-04-29 15:54:35 +0000

    feature: initial implementations of design system components and layout components

---
+++ packages/astro-snipcart-design-system/astro.ts
@@ -0,0 +1,1 @@
+export { default as MinimalistPageLayout } from './src/MinimalistPageLayout.astro';
---
+++ packages/playground/tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+	content: [
+		'../packages/astro-snipcart/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}',
+		'../astro-snipcart-design-system/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}',
+		'./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}

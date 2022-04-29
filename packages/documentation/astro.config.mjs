9805dc18109e8556387eab49fe013a4a34b47968
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-04-29 23:37:20 +0000

    chore: setup initial docs

---
+++ packages/documentation/astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import react from '@astrojs/react';
+
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [// Enable Preact to support Preact JSX components.
+  preact(), // Enable React for the Algolia search component.
+  react(), tailwind(), sitemap()]
+});

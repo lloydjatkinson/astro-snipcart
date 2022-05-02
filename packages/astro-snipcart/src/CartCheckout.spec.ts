51ce6296ca571b6dc4b9f3e70f57f585a55e9647
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-05-02 23:37:05 +0100

    Unit testing (#2)
    * feature: initial setup for unit testing
    
    * build: increase test timeout - this is something that can be removed when astro-component-tester is faster
    
    * chore: version bump

---
+++ packages/astro-snipcart/src/CartCheckout.spec.ts
@@ -0,0 +1,27 @@
+import { describe, it, expect } from 'vitest';
+import { getComponentOutput } from 'astro-component-tester';
+
+describe('CartCheckout.astro', async () => {
+    it('should render as a div by default', async () => {
+        const component = await getComponentOutput('./src/CartCheckout.astro');
+
+        expect(component.html).to.contain('<span');
+    });
+
+    it('should render as the specified element via the polymorphic as prop', async () => {
+        const component = await getComponentOutput('./src/CartCheckout.astro', { as: 'div' });
+
+        expect(component.html).to.contain('<div');
+    });
+
+    it('should render data attribute for snipcart checkout', async () => {
+        const component = await getComponentOutput('./src/CartCheckout.astro');
+
+        expect(component.html).to.contain('snipcart-checkout');
+    });
+
+    it.skip('should render the slotted content', async () => {
+        // const component = await getComponentOutput('./src/CartCheckout.astro');
+        // TODO: astro-component-tester will need to support slots somehow for this to be tested.
+    });
+});

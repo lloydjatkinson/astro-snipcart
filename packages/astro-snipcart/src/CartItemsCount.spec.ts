acda2c93f672ded0706191c66560fafc1a4979f4
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-05-02 22:05:20 +0000

    feature: initial setup for unit testing

---
+++ packages/astro-snipcart/src/CartItemsCount.spec.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from 'vitest';
+import { getComponentOutput } from 'astro-component-tester';
+
+describe('CartItemsCount.astro', async () => {
+    it('should render data attribute for snipcart cart items count', async () => {
+        const component = await getComponentOutput('./src/CartItemsCount.astro');
+
+        expect(component.html).to.contain('snipcart-items-count');
+    });
+});
---
+++ packages/astro-snipcart/test/sanity-test.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+describe('astro snipcart test setup check', () => {
+    it('should pass', () => {
+        expect(true).toBe(true);
+    });
+});

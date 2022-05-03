f071b8b1124638837e6dc1e8032d2f8e4aff1a69
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-05-03 23:24:47 +0000

    chore: add doc url

--- packages/documentation/astro.config.mjs
+++ packages/documentation/astro.config.mjs
@@ -7,6 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://astro-snipcart.vercel.app',
     integrations: [
         // Enable Preact to support Preact JSX components.
         preact(), // Enable React for the Algolia search component.

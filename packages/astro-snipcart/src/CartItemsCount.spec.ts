a33b759af1ea2a2d9a6e666e4db10c2827b87f6c
Lloyd Atkinson <lloydjatkinson@gmail.com>
2024-02-09 00:04:30 +0000

    chore: remove non-functional tests as astro-component-tester depends on an old version of astro

--- packages/astro-snipcart/src/CartItemsCount.spec.ts
+++ packages/astro-snipcart/src/CartItemsCount.spec.ts
@@ -1,10 +1,10 @@
-import { describe, it, expect } from 'vitest';
-import { getComponentOutput } from 'astro-component-tester';
+// import { describe, it, expect } from 'vitest';
+// import { getComponentOutput } from 'astro-component-tester';
 
-describe('CartItemsCount.astro', async () => {
-    it('should render data attribute for snipcart cart items count', async () => {
-        const component = await getComponentOutput('./src/CartItemsCount.astro');
+// describe('CartItemsCount.astro', async () => {
+//     it('should render data attribute for snipcart cart items count', async () => {
+//         const component = await getComponentOutput('./src/CartItemsCount.astro');
 
-        expect(component.html).to.contain('snipcart-items-count');
-    });
-});
+//         expect(component.html).to.contain('snipcart-items-count');
+//     });
+// });
